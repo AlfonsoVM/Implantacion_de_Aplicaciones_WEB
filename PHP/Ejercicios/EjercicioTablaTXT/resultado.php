a0cb323a9a9fa5798c2e80c0b0494fafd27ef665
Alfonso <vargasmesa.alfonso@gmail.com>
2020-02-06 12:54:45 +0100

    EjercicioTablaTXT

---
+++ PHP/Ejercicios/EjercicioTablaTXT/resultado.php
@@ -0,0 +1,5 @@
+<?php
+$uploadedfile = trim(htmlspecialchars($_REQUEST["uploadedfile"], ENT_QUOTES, "UTF-8"));
+
+
+?>
---
+++ PHP/Ejercicios/EjercicioTablaTXT/tratamientodatos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tratamiento Datos</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="uploader.php" method="POST">
+    <table>
+    <tr>
+        <td>Subir tabla (cantidad, precio, total) txt:</td>
+        <td><input name="uploadedfile" type="file"/></td>
+    </tr>
+    <tr><td><br><br><br></td></tr>
+    <tr>
+        <td><input type="submit" value="Subir archivo"/></td>
+    </tr>
+    </table>
+</form>
+</body>
+</html>
